f3ce08fad010b65b92fe476f8a837c36346d1431
Ferrari <ferrarivinicius4@gmail.com>
2024-03-20 22:19:47 -0300

    Criado função de cadastro e exibição de campus e curso

--- sistemaCadastro/src/App.java
+++ sistemaCadastro/src/App.java
@@ -5,16 +5,32 @@ public class App {
     static int quantidadeDeTurma = 0;
     static Campus[] campus = new Campus[50];
     static int quantidadeDeCampus = 0;
+    static Curso[] cursos = new Curso[50];
+    static int quantidadeDeCursos = 0;
+    static Matricula[] matriculas = new Matricula[50];
+    static int quantidadeDeMatricula = 0;
+    static Aluno[] alunos = new Aluno[50];
+    static int quantidadeDeAlunos = 0;
     static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
         
 
         int opcao = 0;
-
+        int opcaoListaCadastrados = 0;
         do {
             exibirMenu();
             opcao = teclado.nextInt();
             roteador(opcao);
+            if (opcao == 6) {
+                exibirListaCadastro();
+                opcaoListaCadastrados = teclado.nextInt();
+                roteadorListas(opcaoListaCadastrados);
+                if (opcaoListaCadastrados == 6) {
+                    exibirMenu();
+                }
+            }
+            
+
         } while (opcao != 0);
     }
 
@@ -30,13 +46,13 @@ public class App {
                 cadastrarMatricula();
                 break;
             case 4:
-                cadastrarEstudante();
+                cadastrarAluno();
                 break;
             case 5:
                 cadastrarTurma();
                 break;
             case 6:
-                exibirTudo();
+                exibirListaCadastro();
                 break;
             default:
             if (opcao !=0) {
@@ -45,27 +61,107 @@ public class App {
                 break;
         }
     }
+    
+    public static void roteadorListas(int opcao) {
+        switch (opcao) {
+            case 1:
+                exibirCampo();
+                break;
+            case 2:
+                exibirCurso();
+                break;
+            case 3:
 
-    private static void exibirTudo() {
+                break;
+            case 4:
+
+                break;
+            case 5:
+                exibirTurma();
+                break;
+            case 6:
+                exibirListaCadastro();
+                break;
+            default:
+                if (opcao != 0) {
+                    System.out.println("Valor invalido");
+                }
+                break;
+        }
+    }
+
+    /* FUNÇÕES PARA EXIBIÇAO DAS LISTAS */
+    private static void exibirCampo() {
+
+        for (int i = 0; i < quantidadeDeCampus; i++) {
+            System.out.println("Campus Cadastrados:");
+            System.out.println("Nome do Campus: " + campus[i].nome);
+            System.out.println("Endereço do Campus: " + campus[i].endereco);
+            System.out.println("Cidade do Campus: " + campus[i].cidade);
+        }
+    }
+    
+    private static void exibirCurso() {
+
+        for (int i = 0; i < quantidadeDeCampus; i++) {
+            System.out.println("Cursos Cadastrados:");
+            System.out.println("Nome do Curso: " + cursos[i].nome);
+            System.out.println("Duração do Curso: " + cursos[i].duracao);
+            System.out.println("Modalidade do Curso: " + cursos[i].modalidade);
+        }
+    }
+    private static void exibirTurma() {
+        
         for (int i = 0; i < quantidadeDeTurma; i++) {
+            System.out.println("Turmas cadastradas:");
             System.out.println("Nome turma: " + turmas[i].nome);
             System.out.println("Numero minimo da turma: " + turmas[i].numeroMinimo);
             System.out.println("Ano de ingresso da turma: " + turmas[i].anoIngresso);
         }
     }
 
-    private static void cadastrarCurso() {;
+    /* FUNÇÕES PARA EXIBIÇAO DAS LISTAS ACABA AQUI*/
+
+
+
+
+
+
+
+    /* FUNÇÕES PARA CADASTRO  */
+    private static void cadastrarCampus() {
+        teclado.nextLine();
+        Campus campo = new Campus();
+        System.out.println("Digite o nome do campus: ");
+        campo.nome = teclado.nextLine();
+        System.out.println("Digite o endereço do campus: ");
+        campo.endereco = teclado.nextLine();
+        System.out.println("Digite a cidade do campus: ");
+        campo.cidade = teclado.nextLine();
+        campus[quantidadeDeCampus] = campo;
+        quantidadeDeCampus++;
     }
 
-    private static void cadastrarEstudante() {
+    private static void cadastrarCurso() {
+        teclado.nextLine();
+        Curso curso = new Curso();
+        System.out.println("Digite o nome do curso: ");
+        curso.nome = teclado.nextLine();
+        System.out.println("Digite a duracao do curso: ");
+        curso.duracao = teclado.nextLine();
+        System.out.println("Digite a modalidade do curso: ");
+        curso.modalidade = teclado.nextLine();
+        cursos[quantidadeDeCursos] = curso;
+        quantidadeDeCursos++;
+    }
+
+    private static void cadastrarAluno() {
     }
 
     private static void cadastrarMatricula() {
     }
 
-    private static void cadastrarCampus() {
-        }
-
+    
     private static void cadastrarTurma() {
         teclado.nextLine();
         Turma turma = new Turma();
@@ -79,6 +175,18 @@ public class App {
         quantidadeDeTurma++;
     }
 
+    /* FUNÇÕES PARA CADASTRO ACABA AQUI */
+
+
+
+
+
+
+
+
+
+
+    /* MENUS */
     public static void exibirMenu() {
         System.out.println("******************************");
         System.out.println("| Digite o valor desejado:   |");
@@ -87,9 +195,24 @@ public class App {
         System.out.println("| 3 - Matricula              |");
         System.out.println("| 4 - Estudante              |");
         System.out.println("| 5 - Turma                  |");
-        System.out.println("| 6 - Exibir tudo            |");
+        System.out.println("| 6 - Exibir menu de listas  |");
         System.out.println("| 0 - Sair                   |");
         System.out.println("******************************");
 
     }
+
+    public static void exibirListaCadastro(){
+        System.out.println("**********************************************");
+        System.out.println("| Digite o valor desejado:                   |");
+        System.out.println("| 1 - Exibir lista de Campus                 |");
+        System.out.println("| 2 - Exibir lista de Curso                  |");
+        System.out.println("| 3 - Exibir lista de Matricula              |");
+        System.out.println("| 4 - Exibir lista de Estudante              |");
+        System.out.println("| 5 - Exibir lista de Turma                  |");
+        System.out.println("| 6 - Voltar ao menu de cadastro             |");
+        System.out.println("| 0 - Sair                                   |");
+        System.out.println("**********************************************");
+    }
+
+    /* MENUS ACABA AQUI */
 }
